2467fcb9405dfc251fb543f3fc0def8833f96339
Mijgona <mijgon.9191@gmail.com>
2021-04-13 03:55:22 +0500

    first version

---
+++ pkg/types/types.go
@@ -0,0 +1,35 @@
+package types
+//Money представляет собой денежную сумму в минимальных единицах(центы, дирамы, копейки) и т.д.
+type Money int64
+
+//Category представляет собой категорию, в которой был совершён платеж (авто, аптеки, рестораны и т.д.)
+type PaymentCategory string
+
+//Status представляет собой статус платежа
+type PaymentStatus string
+
+//Предопределённые статусы платежей
+const(
+	PaymentStatusOk PaymentStatus="OK"
+	PaymentStatusFail PaymentStatus="FAIL"
+	PaymentStatusInProgress PaymentStatus="INPROGRESS"
+)
+
+//Payment представляет информацию о платеже
+type Payment struct {
+	ID 			string	
+	AccountID	int64
+	Amount		Money
+	Category	PaymentCategory
+	Status		PaymentStatus
+}
+
+//Phone хранит номер телефона
+type Phone string
+
+//Account предоставляет информацию о счёте пользователя
+type Account struct{
+	ID			int64
+	Phone		Phone
+	Balance		Money
+}
